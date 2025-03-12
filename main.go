3b3a12be3542505d874000d598e90e6dd573ca75
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-12 02:51:37 -0500

    Added golang modules

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	//connect to database
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//create a router to handle requests
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("/users", createUser(db)).Methods("POST")
+	router.HandleFunc("/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/users/{id}", deleteUser(db)).Methods("DELETE")
+	//start the server
+	log.Fatal(http.ListenAndServe(":8000", jsonContentTypeMiddleware(router)))
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Controllers
+// ------------------------------------------------------------
+// get all users
+func getUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		users := []User{}
+		for rows.Next() {
+			var user User
+			if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+				log.Fatal(err)
+			}
+			json.NewEncoder(w).Encode(users)
+		}
+	}
+}
+
+// get a single user
+func getUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var u User
+		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Email)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				w.WriteHeader(http.StatusNotFound)
+				json.NewEncoder(w).Encode(map[string]string{"error": "User not found"})
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{"error": "Internal Server Error"})
+			}
+			return
+		}
+		json.NewEncoder(w).Encode(u)
+	}
+}
+
+// / create a user
+func createUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var u User
+		json.NewDecoder(r.Body).Decode(&u)
+		err := db.QueryRow("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id", u.Name, u.Email).Scan(&u.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.NewEncoder(w).Encode(u)
+	}
+}
+
+// update a user
+func updateUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+	}
+}
